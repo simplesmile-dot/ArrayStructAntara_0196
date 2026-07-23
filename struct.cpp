f8b10f9f17b0873ec0ff1b5a14943231c61f24d0
Ahmad Zidane Sanduan <zidaneagil5@gmail.com>
2026-07-23 14:24:31 +0700

    Add files via upload

---
+++ struct.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() 
+{
+    //Deklarasi variabel struct
+    Orang mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data Tampilan" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+
+}
